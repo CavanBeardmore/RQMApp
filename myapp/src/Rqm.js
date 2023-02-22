5d9002bd70e088627fdd0ea42e14e54b0b6e2934
CavanBeardmore <106985709+CavanBeardmore@users.noreply.github.com>
2023-02-22 21:24:00 +0000

    Random Quote Machine
    main component for RQM

---
+++ myapp/src/Rqm.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+class Rqm extends React.Component {
+    constructor(props) {
+      super(props);
+    }
+    render() {
+        return (
+          <div>
+          <h1> "The only shots you miss are the ones you don't take. - Wayne Gretzky" - Michael Scott </h1>
+          </div>
+        )
+    }
+  };
+
+
+  export default Rqm;
