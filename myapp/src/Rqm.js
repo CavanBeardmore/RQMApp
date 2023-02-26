f0729c704ce2b04eb84a371183038abb9adbe669
CavanBeardmore <106985709+CavanBeardmore@users.noreply.github.com>
2023-02-26 10:41:56 +0000

    Revert "Revert "Revert "Adding Redux"""
    This reverts commit bda3f52207bb584951cd49b8011204d08c31179c.

--- myapp/src/Rqm.js
+++ myapp/src/Rqm.js
@@ -1,44 +1,17 @@
 import React from "react";
-import './Rqm.scss'
-import { connect } from 'react-redux';
 
 class Rqm extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      input: ""
+    constructor(props) {
+      super(props);
     }
-    
-    this.submitNext = this.submitNext.bind(this);
-  }
-
-  submitNext(){
-    this.props.submitNext()
-    this.setState({
-      input: this.props
-    })
-  }
-
     render() {
         return (
           <div>
-            <h1> "The only shots you miss are the ones you don't take. - Wayne Gretzky" - Michael Scott </h1>
-            <p> "It's Britney bitch" - Michael G. Scott </p>
-
+          <h1> "The only shots you miss are the ones you don't take. - Wayne Gretzky" - Michael Scott </h1>
           </div>
-   )}
+        )
+    }
   };
 
-  const mapStateToProps = (state) => {
-    return {
-      input: state.input
-    }
-  }
 
-  const mapDispatchToProps = (dispatch) => {
-    submitNext: () => {
-      dispatch(nextQuote())
-    }
-  }
-
-  export default connect(mapStateToProps, mapDispatchToProps)(Rqm);
+  export default Rqm;
