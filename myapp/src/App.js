e8b49ea40005b7b8fa030b875af600c2cde38594
CavanBeardmore <106985709+CavanBeardmore@users.noreply.github.com>
2023-02-26 11:58:30 +0000

    adding in methods and JSX

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -9,7 +9,6 @@ class App extends React.Component {
   render() {
       return (
         <div>
-        <h1>Hello React!</h1>
         <Rqm />
         </div>
       )
--- myapp/src/Rqm.js
+++ myapp/src/Rqm.js
@@ -1,16 +1,44 @@
 import React from "react";
+import Styles from './Styles.scss'
 
 class Rqm extends React.Component {
     constructor(props) {
       super(props);
+      this.state = {
+        index: 0,
+      }
+
+      this.handleChange = this.handleChange.bind(this);
+
     }
-    render() {
+    //everytime the button is clicked it increments state by 1
+    handleChange(){
+      this.setState({
+        index: this.state.index + 1
+      });
+    }
+
+    render(){
+
+      const quotesArray = [
+        {quote: 'quote here ', author: '- author'}, 
+        {quote: 'quote here', author: '- author'}
+      ]
+
+      let insertedQuote = '';
+      let insertedAuthor = '';
+
         return (
-          <div>
-          <h1> "The only shots you miss are the ones you don't take. - Wayne Gretzky" - Michael Scott </h1>
+          <body>
+          <header className="title"> Random Quote Generator </header>
+          <div className="quote-box"> 
+            <h1 className="main-quote"> 'Quote' {insertedQuote} </h1>
+            <p className="author"> 'author' {insertedAuthor}</p>
+            <button className="next-button" onClick={this.handleChange()}> Next Quote </button>
           </div>
+          </body>
         )
-    }
+    };
   };
 
 
